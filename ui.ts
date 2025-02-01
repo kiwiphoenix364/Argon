b083579ceb4d26e0df8538965edd189aa7e0cf00
kiwiphoenix <kiwiphoenix364@gmail.com>
2025-02-01 12:19:15 -0500

    Update ui.ts

--- ui.ts
+++ ui.ts
@@ -266,8 +266,14 @@ class Menu {
         d b b c d
         d b c b d
         . d d d .
+    `, img`
+        . . .
+        . . .
+        . . .
+        . . .
+        . . .
     `]
-    public fontNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "~", "`", "<", ">"]
+    public fontNames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", ",", "~", "`", "<", ">", " "]
     constructor(left: number, top: number, width: number, options: string[], itemheight = 7, oppositeAlign = false) {
         this.left = left
         this.top = top
@@ -361,7 +367,7 @@ function editMode() {
     let test: Menu
     controller.moveSprite(cursor)
     controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-        test = new Menu(4, 4, 80, ["test", "test", "hi", "><~`", "0123"])
+        test = new Menu(4, 4, 80, ["test test", "test", "hi", "><~`", "0123"])
         controller.moveSprite(cursor, 0, 0)
     })    
     controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function () {
