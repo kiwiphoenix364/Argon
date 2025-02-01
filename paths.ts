74e069c607c855d0ef645eb1384ba58aaf16c280
kiwiphoenix <kiwiphoenix364@gmail.com>
2025-01-31 20:25:39 -0500

    Initial files for MakeCode project

---
+++ paths.ts
@@ -0,0 +1,40 @@
+class PathPoint {
+    public connectedPoint: PathPoint
+    public x: number
+    public y: number
+    public id: number
+    constructor(x: number, y: number, id: number) {
+        this.x = x
+        this.y = y
+        this.id = id
+    }
+    public renderPoint(image: Image) {
+        image.drawCircle(this.x, this.y, 1, 1)
+        image.drawCircle(this.x, this.y, 3, 1)
+    }
+}
+class Path {
+    public id: number
+    public time: number
+    public pointCounter = 0
+    public pointArray: PathPoint[]
+    constructor(time: number, id: number, pointArray: PathPoint[]) {
+        this.time = time
+        this.id = id
+        this.pointArray = pointArray
+    }
+    public renderPath(image: Image) {
+        let prevItem: PathPoint
+        for (let item of this.pointArray) {
+            if (prevItem) {
+                image.drawLine(item.x, item.y, prevItem.x, prevItem.y, 6)
+            }
+            item.renderPoint(image)
+            prevItem = item
+        }
+    }
+}
+scene.createRenderable(1, (image: Image, camera: scene.Camera) => {
+    let test = new Path(0, 0, [new PathPoint(10, 10, 0), new PathPoint(40, 40, 1)])
+    test.renderPath(image)
+})
