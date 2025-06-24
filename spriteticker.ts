3e85de5fbb3f8b0b262bc92ca293d83ec187695a
kiwiphoenix <kiwiphoenix364@gmail.com>
2025-06-24 00:14:40 -0400

    Update pxt.json, main.ts, spriteticker.ts

---
+++ spriteticker.ts
@@ -0,0 +1,13 @@
+class SpriteLayout {
+    public layoutMatrix: Buffer[]
+    constructor(w: number, h: number) {
+        this.layoutMatrix.fill(Buffer.create(w),0,h)
+        for (let x of this.layoutMatrix) {
+            x.fill(0,0,w)
+        }
+    }
+}
+let spriteUpdater: control.FrameCallback
+spriteUpdater = game.currentScene().eventContext.registerFrameHandler(19, () => {
+
+})
