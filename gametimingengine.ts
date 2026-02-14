ff02e137513135756ae260ce953180cf68708225
kiwiphoenix <kiwiphoenix364@gmail.com>
2026-02-14 15:59:30 -0600

    Update main.blocks, gametimingengine.ts, player.ts, gamemenus.ts

--- gametimingengine.ts
+++ gametimingengine.ts
@@ -3,11 +3,12 @@ let levelData = "{0,0,([10,10,2.356194490192345,40,0][40,40,2.356194490192345,40
 let pathArray: Path[]
 let idCounter: number
 let idCache: number
-let debug = 1
+let debug = 0
 if (debug) {
     EditLevel(levelData)
 } else {
-    RunLevel(levelData)
+    openingMenu()
+    // RunLevel(levelData)
 }
 function EditLevel(levelData: string) {
     game.pushScene()
