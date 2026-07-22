2176e261448bf4af0104533cb79c0ffa9354be48
kiwiphoenix <kiwiphoenix364@gmail.com>
2026-07-22 17:44:01 -0400

    Update dialogengine.ts

--- dialogengine.ts
+++ dialogengine.ts
@@ -6,8 +6,71 @@ class DialogWindow {
         this.dialogBox = boxImg
         this.dialogBoxImage = boxImg.clone()
     }
+    static pause() {
+        pause(40)
+    }
     public clearDialogBox() {
         this.dialogBox = this.dialogBoxImage.clone()
+    }
+    public drawDialog(text: String) {
+        let xPadding = 2
+        let yPadding = 2
+        let pause = 40
+        let keyWords: number[] = []
+        let spaces: number[] = []
+        let spaceIdx: number = 0
+        let emphasized = false
+        let keyWordsLoc = 0
+        let spaceLoc = 0
+        let currentLetterImg: Image
+        let x = xPadding
+        let y = yPadding
+        for (let i = 0; i < text.length; i++) {
+            if (text.charAt(i) === " ") {
+                if (DialogText.emphasizedWords.indexOf(text.slice(spaceIdx + 1, i)) >= 0) {
+                    keyWords.push(spaceIdx)
+                    keyWords.push(i)
+                }
+                spaceIdx = i
+                spaces.push(i)
+            }
+        }
+
+        for (let i = 0; i < text.length; i++) {
+            if (!this.skip) {
+
+                currentLetterImg = DialogText.font[DialogText.fontLetters.indexOf(text.charAt(i).toUpperCase())].clone()
+
+                if (!emphasized && i === keyWords[keyWordsLoc]) {
+                    emphasized = true
+                    keyWordsLoc++
+                }
+                if (emphasized && i === keyWords[keyWordsLoc]) {
+                    emphasized = false
+                    keyWordsLoc++
+                }
+                if (emphasized) {
+                    currentLetterImg.replace(14, 8)
+                }
+
+
+                this.dialogBox.drawTransparentImage(currentLetterImg, x, y)
+                x += currentLetterImg.width + 1
+
+                if (spaces[spaceLoc] === i) {
+                    spaceLoc++
+                }
+                if (x > this.dialogBox.width - xPadding - DialogText.distToSpace(text, i, spaces[spaceLoc])) {
+                    x = xPadding
+                    y += 6
+                }
+
+                DialogWindow.pause()
+            }
+        }
+    }
+    public destroy() {
+        this.dialogBoxImage = this.dialogBox = this.skip = null
     }
 }
 class DialogText {
@@ -294,66 +357,6 @@ class DialogText {
     static getDialog(set: number, value: number) {
         return this.dialog[set][value]
     }
-    static drawDialog(textWindow: DialogWindow, text: String) {
-        let xPadding = 2
-        let yPadding = 2
-        let pause = 40
-        let keyWords: number[] = []
-        let spaces: number[] = []
-        let spaceIdx: number = 0
-        let emphasized = false
-        let keyWordsLoc = 0
-        let spaceLoc = 0
-        let currentLetterImg: Image
-        let x = xPadding
-        let y = yPadding
-        for (let i = 0; i < text.length; i++) {
-            if (text.charAt(i) === " ") {
-                if (this.emphasizedWords.indexOf(text.slice(spaceIdx + 1, i)) >= 0) {
-                    keyWords.push(spaceIdx)
-                    keyWords.push(i)
-                }
-                spaceIdx = i
-                spaces.push(i)
-            }
-        }
-        
-        for (let i = 0; i < text.length; i++) {
-            if (!textWindow.skip) {
-                
-                currentLetterImg = this.font[this.fontLetters.indexOf(text.charAt(i).toUpperCase())].clone()
-                
-                if (!emphasized && i === keyWords[keyWordsLoc]) {
-                    emphasized = true
-                    keyWordsLoc++
-                }
-                if (emphasized && i === keyWords[keyWordsLoc]) {
-                    emphasized = false
-                    keyWordsLoc++
-                }
-                if (emphasized) {
-                    currentLetterImg.replace(14, 8)
-                }
-                
-                
-                textWindow.dialogBox.drawTransparentImage(currentLetterImg, x, y)
-                x += currentLetterImg.width + 1
-
-                if (spaces[spaceLoc] === i) {
-                    spaceLoc++
-                }
-                if (x > textWindow.dialogBox.width - xPadding - this.distToSpace(text, i, spaces[spaceLoc])) {
-                    x = xPadding
-                    y += 6
-                }
-
-                this.pause()
-            }
-        }
-    }
-    static pause() {
-        pause(40)
-    }
     static distToSpace(text: String, startIdx: number, endIdx: number) {
         let space = 0
         for (let i = startIdx; i < endIdx; i++) {
@@ -362,23 +365,25 @@ class DialogText {
         return space
     }
 }
-let mySprite = sprites.create(img`
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-    ................................................................................
-`, SpriteKind.Player)
-let test = new DialogWindow(mySprite.image)
-DialogText.drawDialog(test, DialogText.getDialog(0, 1))
+class DialogController {
+    public sprite: Sprite
+    public dialogWindow: DialogWindow
+    public tree: String[]
+    constructor (left: number, top: number, right: number, bottom: number, tree: String[]) {
+        this.sprite = sprites.create(image.create(right - left, bottom - top), SpriteKind.Player)
+        this.sprite.left = left
+        this.sprite.top = top
+        this.dialogWindow = new DialogWindow(this.sprite.image)
+        this.tree = tree
+        this.drawDialog()
+    }
+    protected drawDialog() {
+        for (let i = 0; i < this.tree.length; i++) {
+            this.dialogWindow.drawDialog(this.tree[i])
+            pauseUntil(() => !controller.A.isPressed(), 0)
+            pauseUntil(() => controller.A.isPressed(), 0)
+            this.dialogWindow.clearDialogBox()
+        }
+    }
+}
+let test2 = new DialogController(0, 0, 160, 120, DialogText.dialog[0])
