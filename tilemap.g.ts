654035f8eafcdda79f0a037be3e6ddcfcb0e15ec
kiwiphoenix <kiwiphoenix364@gmail.com>
2026-07-23 16:12:47 -0400

    Remove transparency16 tile from tilemap

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,7 +1,6 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
 
